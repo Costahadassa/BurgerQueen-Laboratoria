4ac52e021bb66d14d575a5a2bcdf33d7d31b5340
Costahadassa <costahadassa@gmail.com>
2021-02-23 17:00:16 -0300

    funcionalidade de login e cadastro

---
+++ src/Pages/Hall/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function Hall() {
+    return (
+        <>
+        <h1></h1>
+        </>
+    );
+}
+
+export default Hall;
--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,20 +1,44 @@
 import "./styles.css"
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
+import React, { useState } from "react";
+
 
 
 function Login() {
-    return (
+  const [email, setEmail] = useState ('');
+  const [password, setPassword] = useState ('');
+
+  const handleAuthentication = () => {
+    
+    fetch("https://lab-api-bq.herokuapp.com/auth ", {
+      body: `email=${email}&password=${password}` , 
+      method: 'POST',
+      headers: {
+        'accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded' 
+        },
+    },
+    )
+      .then((response) => response.json())
+      .then((result) => {
+        setEmail('')
+        setPassword('')
+        console.log(result)      
+      .catch(error => console.log('error', error));
+      })
+  }
+  return (
     <div id="login-container">
       <h1>Login</h1>
       <form action="">
-        <label for="email">E-mail</label>
-        <input type="email" name="=email" id="email" placeholder="Digite seu e-mail" autocomplete="of"/>
-        <label for="password">Senha</label>
-        <input type="password" name="password" id="password" placeholder="Digite a sua senha"/>
+        <label >E-mail</label>
+        <input type="email" name="=email" value = {email} onChange = {(e) => setEmail(e.target.value)} id="email" placeholder="Digite seu e-mail" autocomplete="of" />
+        <label >Senha</label>
+        <input type="password" name="password" value = {password} onChange = {(e) => setPassword(e.target.value)} id="password" placeholder="Digite a sua senha" />
         <Link to="/register" id="forgot-pass">Não tem cadastro?</Link>
-        <input type="submit" value="Login"/>
-      </form>        
+        <input type="submit" onClick ={() => {handleAuthentication();}} />
+      </form>
     </div>
-    )
+  )
 }
 export default Login;
