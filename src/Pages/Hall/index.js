9c6e05f39416150ecf84371e5d074f5e327a3e93
Costahadassa <costahadassa@gmail.com>
2021-03-04 18:18:00 -0300

    criando pagina hall e suas funcionalidades

--- src/Pages/Hall/index.js
+++ src/Pages/Hall/index.js
@@ -1,25 +1,172 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../App.css';
+import { useHistory } from "react-router-dom";
 
 function Hall() {
-//     const token = localStorage.getItem('token');
-//     var myHeaders = new Headers();
-    
-// myHeaders.append("Authorization", token)
+    const token = localStorage.getItem('token');
+    var myHeaders = new Headers();
 
-// var requestOptions = {
-//   method: 'GET',
-//   headers: myHeaders,
-//   redirect: 'follow'
-// };
+    const history = useHistory();
 
-// fetch("https://lab-api-bq.herokuapp.com/produts",requestOptions)
-//   .then(response => response.text())
-//   .then(result => console.log(result))
-//   .catch(error => console.log('error', error));
+    const [client, setClient] = useState("");
+    const [menu, setMenu] = useState("");
+    const [table, setTable] = useState("");
+    const [order, setOrder] = useState([]);
+    const [menuAlmoco, setMenuAlmoco] = useState("");
+    const [pedido, setPedido] = useState({client:'', table:'', products:[]})
+
+
+
+    // var myHeaders = new Headers();
+    // myHeaders.append("Authorization", token);
+
+    // var requestOptions = {
+    //     method: 'GET',
+    //     headers: myHeaders,
+    //     redirect: 'follow'
+    // };
+    console.log(token)
+    // useEffect(() => {
+    //     fetch("https://lab-api-bq.herokuapp.com/products", {
+    //         headers: {
+
+    //             'accept': 'application/json',
+    //             'Authorization': `${token}`
+    //         },
+    //     })
+    //         .then(response => response.json())
+    //         .then(result => console.log(result))
+    //         .catch(error => console.log('error', error));
+    // }, [token])
+
+    // const breakfast = () => { };
+    // const burger = () => { };
+    // const sendKitchen = () => { };
+    function getCafe() {
+        console.log('ok')
+        fetch("https://lab-api-bq.herokuapp.com/products", {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `${token}`
+            }
+        })
+            .then(response => response.json())
+            .then(result => {
+                console.log(result)
+                const cafeDamanha = result.filter(item => item.type === 'breakfast')
+                setMenu(cafeDamanha)
+
+            })
+            .catch(error => console.log('error', error))
+    }
+    function getAlmoco() {
+        console.log('ok')
+        fetch("https://lab-api-bq.herokuapp.com/products", {
+            headers: {
+
+                'accept': 'application/json',
+                'Authorization': `${token}`
+            },
+        })
+            .then(response => response.json())
+            .then(result => {
+                const almoco = result.filter(item => item.type === 'all-day')
+                setMenuAlmoco(almoco)
+            })
+            .catch(error => console.log('error', error));
+    }
+    function addItem(e) {
+        const parent = e.target.parentNode
+        const price = parent.getAttribute('price')
+        const name = parent.getAttribute('name')
+        const id = parent.getAttribute('id')
+        const flavor = parent.getAttribute('flavor')
+        const complement = parent.getAttribute('complement')
+        const itemPedido = {
+            id: id,
+            name: name,
+            price: price,
+            qtd: 1,
+            flavor: flavor,
+            complement: complement
+        }
+        console.log(order)
+        if (!order.some(item => item.id === id)) {
+            setOrder([...order, itemPedido])
+        }
+        else {
+            order.map((item, index) => {
+                if (item.id === id) {
+                    order[index].qtd++
+                    setOrder([...order])
+
+                }
+            })
+        }
+    }
+    function handlePedido(){
+        const products = order.map(item=>{
+            return {'id':item.id,'qtd':item.qtd}
+        })
+        pedido.products=products
+        console.log(pedido)
+        fetch("https://lab-api-bq.herokuapp.com/orders", {
+            method:'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `${token}`
+            },
+            body:JSON.stringify(pedido)
+        })
+        .then(response=>response.json())
+        .then(data=>{
+            console.log(data)
+        })
+    }
     return (
         <>
-        <h1>ola</h1>
+        <input type='text'placeholder='Nome do Cliente'onChange={(e)=>setPedido({...pedido,'client':e.target.value})}></input>
+        <input type='text'placeholder='Mesa'onChange={(e)=>setPedido({...pedido,'table':e.target.value})}></input>
+            <button onClick={getCafe}>Café da manhã</button>
+            <div>
+                {menu && menu.map((item) => (
+                    <div price={item.price} name={item.name} id={item.id} onClick={addItem}>
+                        <p>
+                            {item.name}
+                        </p>
+                        <p>
+                            {item.price}
+                        </p>
+                    </div>
+                ))}
+            </div>
+            <button onClick={getAlmoco}>Almoço</button>
+            <div>
+                {menuAlmoco && menuAlmoco.map((item) => (
+                    <div price={item.price} name={item.name} id={item.id} flavor={item.flavor} complement={item.complement} onClick={addItem}>
+                        <p>
+                            {item.name} {item.flavor} {item.complement}
+                        </p>
+                        <p>
+                            {item.price}
+                        </p>
+                    </div>
+                ))}
+            </div>
+            <div>
+                <h1>Resumo Pedido</h1>
+                {order && order.map((item) => (
+                    <div id={item.id}>
+                        <p>
+                            {item.qtd}x {item.name} {item.flavor} {item.complement}
+                        </p>
+                        <p>
+                            R${item.price}
+                        </p>
+                    </div>
+                ))}
+            </div>
+            <button onClick={handlePedido}>Enviar Pedido</button>
         </>
     );
 }
--- src/Pages/Kitchen/index.js
+++ src/Pages/Kitchen/index.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../App.css';
 
 function Kitchen() {
-    console.log("vc esta na cozinha")
+    const token = localStorage.getItem("token")
+    const id = localStorage.getItem("id")
+    console.log(token, id)
+    const [pendentes, setPendentes] = useState([])
+
+    useEffect(() => {
+        fetch("https://lab-api-bq.herokuapp.com/orders", {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `${token}`
+            },
+        })
+            .then(response => response.json())
+            .then(result => {
+                const pedidosPendentes=result
+                setPendentes(pedidosPendentes.filter((pedido)=> pedido.status.includes('pending')))               
+
+            })
+            .catch(error => console.log('error', error));
+    },[token])
+    console.log(pendentes)    
     return (
-        <>
-        <h1>coz</h1>
-        </>
+        <div className="Cozinha">
+            <h1 className="tituloCozinha">cozinha</h1>
+            {pendentes.map(pedido=>(
+                <>
+                <p>{pedido.client_name}</p>
+                <div>{pedido.Products.map(item=>(
+                    <p>{item.name}</p>
+                ))}</div>
+                </>
+            ))}
+        </div>
     );
 }
 
